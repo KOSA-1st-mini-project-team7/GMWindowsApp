e5859c5ac95e32987a293451532d1ae4f4c0274f
havegrit <shin_jaean@naver.com>
2024-06-04 12:27:14 +0900

    delete: 불필요한 파일 제거

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,16 @@
 module org.example.gymmanagementapp {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.jfoenix;
     requires org.controlsfx.controls;
-    requires org.kordamp.bootstrapfx.core;
     requires java.sql;
+    requires static lombok;
 
     opens org.example.gymmanagementapp to javafx.fxml;
+    opens org.example.gymmanagementapp.users.controller to javafx.fxml;
+    opens org.example.gymmanagementapp.member.controller to javafx.fxml;
+
     exports org.example.gymmanagementapp;
+    exports org.example.gymmanagementapp.users.controller;
+    exports org.example.gymmanagementapp.member.controller;
 }
