33a2161fd7244f620834a388a8fe51e6ed43ca1c
havegrit <shin_jaean@naver.com>
2024-06-10 15:26:20 +0900

    [refactor] DML 작업시 리소스 관리를 위한 리팩토링 작업

--- src/main/java/org/example/gymmanagementapp/util/DBManager.java
+++ src/main/java/org/example/gymmanagementapp/util/DBManager.java
@@ -8,21 +8,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBManager {
-    public ResultSet executeQuery(String sql, Object... parameters) throws SQLException {
-        Connection conn = DBConfig.getInstance().getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        for (int i = 0; i < parameters.length; i++) {
-            stmt.setObject(i + 1, parameters[i]);
+    public static ResultSet executeQuery(String sql, Object... parameters) throws SQLException {
+        Connection conn;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DBConfig.getInstance().getConnection();
+            stmt = conn.prepareStatement(sql);
+            for (int i = 0; i < parameters.length; i++) {
+                stmt.setObject(i + 1, parameters[i]);
+            }
+            rs = stmt.executeQuery();
+            // 결과를 호출한 곳에서 처리할 수 있도록 반환
+            return rs;
+        } catch (SQLException e) {
+            if (rs != null) try { rs.close(); } catch (SQLException ignore) {}
+            if (stmt != null) try { stmt.close(); } catch (SQLException ignore) {}
+            throw e;
         }
-        return stmt.executeQuery();
     }
 
-    public int executeUpdate(String sql, Object... parameters) throws SQLException {
-        Connection conn = DBConfig.getInstance().getConnection();
-        PreparedStatement stmt = conn.prepareStatement(sql);
-        for (int i = 0; i < parameters.length; i++) {
-            stmt.setObject(i + 1, parameters[i]);
+    public static int executeUpdate(String sql, Object... parameters) throws SQLException {
+        try {
+            Connection conn = DBConfig.getInstance().getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            for (int i = 0; i < parameters.length; i++) {
+                stmt.setObject(i + 1, parameters[i]);
+            }
+            return stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return stmt.executeUpdate();
+        return 0;
     }
 }
